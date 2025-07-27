604e83138520a84a2e9ee8c50eb07e27b3f0d714
cat-bucket <gitbit843@gmail.com>
2025-07-27 13:40:21 +0800

    Update Logger.cpp

--- model_training/src/io/Logger.cpp
+++ model_training/src/io/Logger.cpp
@@ -1,7 +1,12 @@
 
 #include "io/Logger.h"
 #include <iostream>
+#include <fstream>
+#include <ctime>
 
 void Logger::print(const char *msg) {
-    std::cout << "[NEURAL_NET] " << msg << std::endl;
+    std::ofstream log("training_log.txt", std::ios::app);
+    std::time_t now = std::time(nullptr);
+    log << "[NEURAL_NET " << std::ctime(&now) << "] " << msg << std::endl;
+    log.close();
 }
